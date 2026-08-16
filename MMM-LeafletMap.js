24691db53f2197cef901bc0d828c6dce9e06c222
ha2013210 <ha2013210@gmail.com>
2026-08-16 09:06:52 +0300

    Add files via upload

--- MMM-LeafletMap.js
+++ MMM-LeafletMap.js
@@ -1,11 +1,14 @@
 Module.register("MMM-LeafletMap", {
     defaults: {
-        latitude: 51.505,
-        longitude: -0.09,
+        latitude: 29.13, // Default to your Kuwait coordinates!
+        longitude: 48.11,
         zoom: 13,
         height: "300px",
         width: "100%"
     },
+
+    // Save a global reference to the map engine instance
+    mapInstance: null,
 
     getStyles: function() {
         return ["leaflet.css"];
@@ -23,9 +26,9 @@ Module.register("MMM-LeafletMap", {
         return wrapper;
     },
 
-        notificationReceived: function(notification, payload, sender) {
+    notificationReceived: function(notification, payload, sender) {
         if (notification === "DOM_OBJECTS_CREATED") {
-            // FIX: Tell Leaflet exactly where your local images folder is
+            // Tell Leaflet exactly where your local images folder is
             delete L.Icon.Default.prototype._getIconUrl;
             L.Icon.Default.mergeOptions({
                 iconRetinaUrl: this.file("images/marker-icon-2x.png"),
@@ -33,18 +36,27 @@ Module.register("MMM-LeafletMap", {
                 shadowUrl: this.file("images/marker-shadow.png"),
             });
 
-            // Start the Leaflet engine using our local files
-            var map = L.map('leaflet-map-container').setView([this.config.latitude, this.config.longitude], this.config.zoom);
+            // Start the Leaflet engine and save it to our global variable
+            this.mapInstance = L.map('leaflet-map-container').setView([this.config.latitude, this.config.longitude], this.config.zoom);
 
-            // Pull free OpenStreetMap map tiles from the web
+            // Pull free OpenStreetMap map tiles from the web securely over HTTPS
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '&copy; OpenStreetMap'
-            }).addTo(map);
+            }).addTo(this.mapInstance);
 
-            // Place a pin drop on the map
-            L.marker([this.config.latitude, this.config.longitude]).addTo(map);
+            // Place your pin drop on the map
+            L.marker([this.config.latitude, this.config.longitude]).addTo(this.mapInstance);
+        }
+
+        // FIX FOR MMM-PAGES: Force Leaflet to recalculate when the page becomes visible
+        if (notification === "PAGE_CHANGED" || notification === "MODULE_DOM_UPDATED") {
+            var self = this;
+            setTimeout(function() {
+                if (self.mapInstance) {
+                    self.mapInstance.invalidateSize();
+                }
+            }, 300); // Give the browser 300ms to animate the page change first
         }
     }
 });
-
